639e70a5c512916dec5cea31ebdd2935dd02a572
agl-c <55969133+agl-c@users.noreply.github.com>
2022-04-06 20:22:33 +0800

    try to add dp

--- tests/trials.py
+++ tests/trials.py
@@ -1,15 +1,21 @@
 from tdigest import TDigest
 from numpy.random import random
-import sys
-
-'\n'.join(sys.path)
-print(sys.path)
 
 digest = TDigest()
-for x in range(500):
+for x in range(50):
     digest.update(random())
 
 print(digest.percentile(15))  # about 0.15, as 0.15 is the 15th percentile of the Uniform(0,1) distribution
 
-print(digest)
+# print(digest.C)
+centroids = digest.centroids_to_list()
+print(centroids)
 
+print("******************* Then we add noise *********************")
+digest.anonymize()
+
+noisy_centroids = []
+for key in digest.noisy_C.keys():
+    tree_values = digest.noisy_C.get_value(key)
+    noisy_centroids.append({'m':tree_values.mean, 'c':tree_values.count})
+print(noisy_centroids)
